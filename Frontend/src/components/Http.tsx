0f7c4fd22ba909292bc8378ed2e09806a7784d9a
AlaaAbbas22 <alaa@uni.minerva.edu>
2024-05-03 12:32:50 +0300

    edit for deployment

--- Frontend/src/components/Http.tsx
+++ Frontend/src/components/Http.tsx
@@ -1,3 +1,3 @@
 import axios from "axios";
 //headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
-export default axios.create({withCredentials: true,headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}});
+export default axios.create({withCredentials: true});
